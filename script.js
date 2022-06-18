5f73dbf6bd8f23b7331ed8a4762b50b0bfa817c3
mitchgm <mitchgm@bgsu.edu>
2022-06-17 20:17:13 -0700

    starting javascript and functions

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
 // Assignment code here
+var specialCharactersArray = [
+    '',
+    '!',
+    '"',
+    '#',
+    '$',
+    '%',
+    '&',
+    '()',
+    '*',
+    '+',
+    ',',
+    '-',
+    '.',
+    '/',
+    ':',
+    ';',
+    '<',
+    '=',
+    '>',
+    '?',
+    '@',
+    '[',
+    ']',
+    '^',
+    
+];
+
+var generatePassword = function() {
+    var passwordLength = window.prompt('How many characters would you like your password to be? ');
+
+        passwordLength = passwordLength.toLocaleLowerCase();
+
+        if (passwordLength > 7 && passwordLength < 128) {
+           var passwordLowChar = window.prompt('What character types would you like in the password? Would you like to include lowercase letters? Type yes or no. ');
+           passwordLowChar = passwordLowChar.toLocaleLowerCase();
+           if (passwordLowChar === "yes") {
+
+           }
+
+            else if (passwordLowChar === "no") {
+
+            }  
+
+            else  {
+
+            }
+            
+            
+        }
+
+        else {
+            window.alert("You need to enter a valid password length between 8 and 128 characters")
+            
+        }
+};
 
 
 // Get references to the #generate element
