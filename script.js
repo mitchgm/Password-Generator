3666644de58010e1739efcd94745a84d27d690bc
mitchgm <mitchgm@bgsu.edu>
2022-06-18 10:57:38 -0700

    not much progress

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
 // Assignment code here
 var specialCharactersArray = [
-    '',
-    '!',
-    '"',
-    '#',
-    '$',
-    '%',
-    '&',
-    '()',
-    '*',
-    '+',
-    ',',
-    '-',
-    '.',
-    '/',
-    ':',
-    ';',
-    '<',
-    '=',
-    '>',
-    '?',
-    '@',
-    '[',
-    ']',
-    '^',
+    "",
+    "!",
+    "'",
+    "#",
+    "$",
+    "%",
+    "&",
+    "()",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^",
     
 ];
+
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ];
+var lowCharArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var highCharArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
 var passwordLength, passwordLowChar, passwordHighChar, passwordNum, passwordSpecial, thePassword;
 
 
-// start of generate password function
-function generatePassword() {
-    var passwordLength = window.prompt('How many characters would you like your password to be? ');
+function generatePassword () {
+         getPasswordLength ();
+     //getPasswordLowChar ();
+          // getPasswordHighChar ();
+         //  getPasswordNum ();
+          // getPasswordSpecial ();
+          getThePassword ();
+        
+};
+
+// start of password length function
+function getPasswordLength() {
+    passwordLength = window.prompt("How many characters would you like your password to be? ");
 
         passwordLength = passwordLength.toLocaleLowerCase();
 
         if (passwordLength > 7 && passwordLength < 128) {
-           //getPasswordLowChar ();
-          // getPasswordHighChar ();
-         //  getPasswordNum ();
-          // getPasswordSpecial ();
-
-           for (var i = 0; i < passwordLength; i++) {
-            thePassword = Math.floor(Math.random() * specialCharactersArray.passwordLength);
-            console.log(specialCharactersArray[thePassword]);
-
-           }
+           return passwordLength = passwordLength;
+           
         }
 
         else {
@@ -59,7 +65,7 @@ function generatePassword() {
 
 // start of lowercase function
 function getPasswordLowChar() {
-    passwordLowChar = window.prompt('What character types would you like in the password? Would you like to include lowercase letters? Type yes or no. ');
+    passwordLowChar = window.prompt("What character types would you like in the password? Would you like to include lowercase letters? Type yes or no. ");
    passwordLowChar = passwordLowChar.toLocaleLowerCase();
    if (passwordLowChar === "yes") {
     return passwordLowChar = true;
@@ -81,7 +87,7 @@ function getPasswordLowChar() {
 
 // start of uppercase function
 function getPasswordHighChar() {
-    passwordHighChar =  window.prompt('Would you like to include uppercase letters? Type yes or no. ');
+    passwordHighChar =  window.prompt("Would you like to include uppercase letters? Type yes or no. ");
     passwordHighChar = passwordHighChar.toLocaleLowerCase();
     if (passwordHighChar === "yes") {
      return passwordHighChar = true;
@@ -103,7 +109,7 @@ function getPasswordHighChar() {
 
 // start of number function
 function getPasswordNum () {
-    passwordNum = window.prompt('Would you like to include numbers? Type yes or no. ');
+    passwordNum = window.prompt("Would you like to include numbers? Type yes or no. ");
     passwordNum = passwordNum.toLocaleLowerCase();
     if (passwordNum === "yes") {
         return passwordNum = true;
@@ -123,7 +129,7 @@ function getPasswordNum () {
 
 // start of special character function
 function getPasswordSpecial () {
-    passwordSpecial = window.prompt('Would you like to include special characters? Type yes or no. ');
+    passwordSpecial = window.prompt("Would you like to include special characters? Type yes or no. ");
     passwordSpecial = passwordSpecial.toLocaleLowerCase();
     if (passwordSpecial === "yes") {
         return passwordSpecial = true;
@@ -140,7 +146,15 @@ function getPasswordSpecial () {
 
 };
 
+// start for loop get password function
+function getThePassword () {
+    for (var i = 0; i < passwordLength; i++) {
+                
+        thePassword = (specialCharactersArray[Math.floor(Math.random() * passwordLength)]);
+        console.log(thePassword)
 
+    }
+};
 
 
 // Get references to the #generate element
@@ -150,13 +164,13 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-    console.log(passwordLowChar);
+     console.log(passwordLength);
+     console.log(passwordLowChar);
     console.log(passwordHighChar);
     console.log(passwordNum);
     console.log(passwordSpecial);
-  passwordText.value = password;
-
-}
+    
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
